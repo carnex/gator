f3d174073e133ca96e4ef0dea362ce945bac267c
carnex <perkoset43@gmail.com>
2026-02-23 20:55:58 +0000

    added get user and system reset functions

--- handlers.go
+++ handlers.go
@@ -15,10 +15,14 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 	name := cmd.Args[0]
-
-	err := s.cfg.SetUser(name)
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, name)
 	if err != nil {
-		return fmt.Errorf("couldn't set current user: %w", err)
+		return err
+	}
+	errr := s.cfg.SetUser(name)
+	if errr != nil {
+		return fmt.Errorf("couldn't set current user: %w", errr)
 	}
 
 	fmt.Println("User switched successfully!")
@@ -36,5 +40,35 @@ func handlerRegister(s *state, cmd command) error {
 	if err != nil {
 		return err
 	}
+	s.cfg.SetUser(insertedUser.Name)
+	fmt.Printf("%s was created", insertedUser.Name)
 
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	ctx := context.Background()
+	err := s.db.DeleteUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+	fmt.Printf("DB users deleted")
+	return nil
+}
+
+func handlerUsers(s *state, cmd command) error {
+	ctx := context.Background()
+	users, err := s.db.GetUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("unable to retrive users %w", err)
+	}
+	//currentUser := s.cfg.CurrentUserName
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("%s (current)\n", user.Name)
+		} else {
+			fmt.Printf("%s\n", user.Name)
+		}
+	}
+	return nil
 }
